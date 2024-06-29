0bc64b3c81f6c65ef4c1d28e5d9229230b46aacb
apsamade <bouderga.abdessamade@gmail.com>
2024-06-29 15:33:12 +0200

    maj uploadthings

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -16,14 +16,14 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ titre: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          name: input.titre,
           createdBy: { connect: { id: ctx.session.user.id } },
         },
       });
