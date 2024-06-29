048a96687ed5374c54fed18a599c5ed828ea0378
apsamade <bouderga.abdessamade@gmail.com>
2024-06-29 18:09:11 +0200

    maj nextauth

--- src/server/db.ts
+++ src/server/db.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma/client";
 import { env } from "@/env";
 
 const createPrismaClient = () =>
-  new PrismaClient();
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
